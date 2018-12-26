51c94ac0f4bdb8e2f7c6e78a8c713457ebcb0d16
MAYUR ANDHARE <mayurandhare13@gmail.com>
2018-12-26 13:48:30 -0800

    fixed indentation

--- Linked Lists/RemoveDups.java
+++ Linked Lists/RemoveDups.java
@@ -46,8 +46,8 @@ class RemoveDups
     public static void main(String[] args) 
     {
         LinkedListNode first = new LinkedListNode(0, null, null);
-		LinkedListNode head = first;
-		LinkedListNode second = first;
+        LinkedListNode head = first;
+        LinkedListNode second = first;
         for (int i = 1; i < 8; i++) 
         {
 			second = new LinkedListNode(i % 2, null, null);
