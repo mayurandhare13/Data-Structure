a06848d32746e44ed207485f4c2bf61b9469066b
MAYUR ANDHARE <mayurandhare13@gmail.com>
2018-12-26 13:46:52 -0800

    Linked List Solutions

---
+++ Linked Lists/LinkedListNode.java
@@ -0,0 +1,55 @@
+public class LinkedListNode
+{
+    public LinkedListNode next;
+    public LinkedListNode prev;
+    public LinkedListNode last;
+    public int data;
+
+    public LinkedListNode() {}
+
+    public LinkedListNode(int d)
+        { data = d; }
+
+    public LinkedListNode(int d, LinkedListNode n, LinkedListNode p)
+    {
+        data = d;
+        setNext(n);
+        setPrev(p);
+    }
+
+    public void setNext(LinkedListNode n)
+    {
+        next = n;
+        if(this == last)
+            last = n;
+        if(n != null && n.prev != this)
+            n.setPrev(this);
+    }
+
+    public void setPrev(LinkedListNode p)
+    {
+        prev = p;
+        if(p != null && p.next != this)
+            p.setNext(this);
+    }
+
+    public String printForward()
+    {
+        if(next != null)
+            return data+" -> "+next.printForward();
+        else
+            return ((Integer)data).toString();
+        
+    }
+
+    public LinkedListNode clone()
+    {
+        LinkedListNode next2 = null;
+        if(next != null)
+            next2 = next.clone();
+        
+        LinkedListNode head2 = new LinkedListNode(data, next2, null);
+
+        return head2;
+    }
+}
--- Linked Lists/RemoveDups.java
+++ Linked Lists/RemoveDups.java
@@ -1,15 +1,14 @@
 // Que - Remove Duplicates from list
-
+import LinkedListNode;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
 
 class RemoveDups
 {
-    void deleteDups(LinkedList n)
+    void deleteDups(LinkedListNode n)
     {
-        HashSet<Integer> set = new HashSet<>();
-        LinkedList prev = null;
+        HashSet<Integer> set = new HashSet<Integer>();
+        LinkedListNode prev = null;
         while(n != null)
         {
             if(set.contains(n.data))
@@ -18,11 +17,51 @@ class RemoveDups
             }
             else
             {
-                set.add(n.add);
+                set.add(n.data);
                 prev = n;
             }
             n = n.next;
         }
     }
     // O(N) time ==> N number of elements in list
+
+    void deleteDups2(LinkedListNode head)
+    {
+        LinkedListNode current = head;
+        while(current != null)
+        {
+            LinkedListNode runner = current;
+            while(runner.next != null)
+            {
+                if(runner.next.data == current.data)
+                    runner.next = runner.next.next;
+                else
+                    runner = runner.next;
+            }
+            current = current.next;
+        }
+    }
+    // O(1) space complexity, but O(n^2) time.
+
+    public static void main(String[] args) 
+    {
+        LinkedListNode first = new LinkedListNode(0, null, null);
+		LinkedListNode head = first;
+		LinkedListNode second = first;
+        for (int i = 1; i < 8; i++) 
+        {
+			second = new LinkedListNode(i % 2, null, null);
+			first.setNext(second);
+			second.setPrev(first);
+			first = second;
+		}
+		
+        System.out.println(head.printForward());
+
+        RemoveDups rd = new RemoveDups();
+        rd.deleteDups2(head);
+
+        System.out.println(head.printForward());
+
+    }
 }
