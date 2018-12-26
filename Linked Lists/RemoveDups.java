342f12ab944326e0bf2ab6998132ef1094aafa7e
MAYUR ANDHARE <mayurandhare13@gmail.com>
2018-12-25 23:50:34 -0800

    LinkedList Solutions

---
+++ Linked Lists/RemoveDups.java
@@ -0,0 +1,27 @@
+// Que - Remove Duplicates from list
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class RemoveDups
+{
+    void deleteDups(LinkedList n)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        LinkedList prev = null;
+        while(n != null)
+        {
+            if(set.contains(n.data))
+            {
+                prev.next = n.next;
+            }
+            else
+            {
+                set.add(n.add);
+                prev = n;
+            }
+            n = n.next;
+        }
+    }
+}
