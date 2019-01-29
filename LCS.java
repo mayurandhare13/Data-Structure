c56b79389c2f8be4f7e2a5e64642c79cf7594f08
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-01-28 20:20:38 -0800

    added backtrack for common sequence

--- LCS.java
+++ LCS.java
@@ -26,17 +26,46 @@ class LCS
             System.out.println(Arrays.toString(r));
         }
 
-    //    printSubs(mat, s1, s2, max);  TODO
+        printSubs(mat, s1, s2, max);
 
         return max;
     }
 
+    private static void printSubs(int[][] mat, char[] s1, char[] s2, int max)
+    {
+        char[] subs = new char[max];
+        
+        int i = mat.length-1, j = mat[0].length-1;
+        while(mat[i][j] != 0)
+        {
+            if(mat[i][j] == Math.max(mat[i-1][j], mat[i][j-1]))
+            {
 
+                if(mat[i][j] == mat[i][j-1])
+                    j = j-1;
+                else
+                    i = i-1;  
+            }
+            else if(mat[i][j] == mat[i-1][j] && mat[i][j] == mat[i][j-1])
+            {
+                i -= 1;
+            }
+            else if(mat[i][j] > Math.max(mat[i-1][j], mat[i][j-1]))
+            {
+                subs[--max] = s1[i-1];
+
+                i--;
+                j--;
+            }
+        }
+
+        System.out.println(subs);
+    }
 
     public static void main(String[] args) 
     {
-        String s1 = "abcdaf";
-        String s2 = "acbcf";
+        String s1 = "abcgtdaf";
+        String s2 = "acgtbdcf";
         int maxLen = findSubs(s1.toCharArray(), s2.toCharArray()); 
         System.out.println(maxLen);
     }
