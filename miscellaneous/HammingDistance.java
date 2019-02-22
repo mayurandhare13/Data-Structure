522a50834ec9ef01f37ecb303117895225cad20b
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-02-22 13:11:42 -0800

    leet code

---
+++ miscellaneous/HammingDistance.java
@@ -0,0 +1,19 @@
+class HammingDistance
+{
+    public static int hammingDistance(int x, int y)
+    {
+        int r = x ^ y;
+        int count = 0;
+        while(r > 0)
+        {
+            count += r & 1;
+            r >>= 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println(hammingDistance(1, 4));    
+    }
+}
