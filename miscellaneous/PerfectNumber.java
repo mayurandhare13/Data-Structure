531c65198ad6abc0bce765389eba8340f24567f5
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-02-22 20:23:51 -0800

    leet code

---
+++ miscellaneous/PerfectNumber.java
@@ -0,0 +1,23 @@
+class PerfectNumber
+{
+    public static boolean checkPerfectNumber(int num) 
+    {
+        if(num <= 1)
+            return false;
+        
+        int sum = 1;
+        int maxNum = (int)Math.sqrt(num);
+        for (int i = 2; i <= maxNum; i++) 
+        {
+            if (num % i == 0)
+                sum += i + num / i;
+        }
+        
+        return sum == num;
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println(checkPerfectNumber(28));    
+    }
+}
