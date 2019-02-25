102dfe03b1c124d884b65f3cfd16b6c9f67302c0
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-02-24 23:53:24 -0800

    Daily Coding Practice

---
+++ Daily Coding Practice/StringDecode.py
@@ -0,0 +1,18 @@
+def stringDecoode(digits):
+    n = len(digits)
+    count = [0] * (n+1)
+    count[0] = 1
+    count[1] = 1
+
+    for i in range(2, n+1):
+        count[i] = 0
+        if(digits[i-1] > '0'):
+            count[i] = count[i-1]
+
+        if(digits[i-2] == '1' or digits[i-2] == '2' and digits[i-2] < '7'):
+            count[i] += count[i-2]
+
+    return count[n]
+
+digits = "12315"
+print("count is: ", stringDecoode(digits))
