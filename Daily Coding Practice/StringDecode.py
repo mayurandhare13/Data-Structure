7ccbea273576516e8bec3b7b7b63e02b82dddf4f
MAYUR ANDHARE <mayurandhare13@gmail.com>
2019-02-24 23:55:24 -0800

    Daily Coding Practice

--- Daily Coding Practice/StringDecode.py
+++ Daily Coding Practice/StringDecode.py
@@ -9,7 +9,7 @@ def stringDecoode(digits):
         if(digits[i-1] > '0'):
             count[i] = count[i-1]
 
-        if(digits[i-2] == '1' or digits[i-2] == '2' and digits[i-2] < '7'):
+        if(digits[i-2] == '1' or digits[i-2] == '2' and digits[i-1] < '7'):
             count[i] += count[i-2]
 
     return count[n]
